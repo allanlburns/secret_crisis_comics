75fccdfecfcce8147f9127dcdc031289651f7a3c
Allan Burns <allanlburns@yahoo.com>
2019-12-02 20:50:04 -0500

    Initialized db and added several sample products

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
-from app import app
+from app import app, db
 from flask import render_template, url_for, redirect
+from app.models import Product
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title="Home")
+
+@app.route('/products', methods={'GET'})
+def products():
+    return render_template('index.html', title="Products")
