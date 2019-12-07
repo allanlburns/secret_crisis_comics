fd5918949f196b5f33ac88bd3f6bfeead3083b56
Allan Burns <allanlburns@yahoo.com>
2019-12-07 15:26:26 -0500

    User model created, and user can register, login, and logout. Header nav links display dynamically based on user login. TODO: Create New Arrivals page, fetch new arrivals with API and display on page.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
-from app import app, db     #, login
+from app import app, db, login
 from datetime import datetime
-# from flask_login import UserMixin
-# from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +9,21 @@ class Product(db.Model):
     image = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(500), nullable=False)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    username = db.Column(db.String(30), unique=True, index=True)
+    email = db.Column(db.String(120), unique=True, index=True)
+    password_hash = db.Column(db.String(256))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)    
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,83 @@
 from app import app, db
-from flask import render_template, url_for, redirect
-from app.models import Product
+from flask import render_template, url_for, redirect, flash
+from app.models import Product, User
+from app.forms import LoginForm, RegisterForm
+from flask_login import login_user, logout_user, login_required, current_user
+import datetime
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods={'GET'})
 def index():
-    return render_template('index.html', title="Home")
+    products = Product.query.all()
+    return render_template('index.html', title="Home", products=products)
 
 @app.route('/products', methods={'GET'})
 def products():
     products = Product.query.all()
     return render_template('products.html', title="Products", products=products)
+
+@app.route('/login', methods={'GET', 'POST'})
+def login():
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        # qurey the db for the user information and log them in if everything is valid.
+        user = User.query.filter_by(email=form.email.data).first()
+
+        # if user doesn't exsit, reload page and flash message
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid credentials')
+            return redirect(url_for('login'))
+
+        #if user exists and credentials are correct, log them in
+        login_user(user)
+        flash(f"You have been logged in.")
+        return redirect(url_for('profile', username=current_user.username))
+
+    return render_template('form.html', form=form, title='Login')
+
+@app.route('/register', methods={'GET', 'POST'})
+def register():
+    # if user is already logged in , redirect them.
+    if current_user.is_authenticated:
+        flash("you are already logged in")
+        return redirect(url_for('profile', username=current_user.username))
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        user = User(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            username = form.username.data,
+            email = form.email.data,
+        )
+
+        # include password
+        user.set_password(form.password.data)
+
+        # add to stage and commit
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f"You have been registered.")
+        return redirect(url_for('login'))
+
+    return render_template('form.html', form=form, title='Register')
+
+@app.route('/profile')
+@app.route('/profile/<username>', methods=['GET', 'POST'])
+def profile(username=''):
+    # if username is empty
+    if not username:
+        return redirect(url_for('login'))
+
+    person = User.query.filter_by(username=username).first()
+
+    return render_template('profile.html', title='Profile', person=person)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
