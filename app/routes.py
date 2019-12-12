d1a28a52fe83dabaf447b179f40c4df8498b5b2b
Allan Burns <allanlburns@yahoo.com>
2019-12-12 12:27:46 -0500

    Able to add products to cart using Flask sessions, created route and view to display products in cart on table. TODO: integrate Stripe checkout component.

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,73 @@
 from app import app, db
-from flask import render_template, url_for, redirect, flash, jsonify
+from flask import render_template, url_for, redirect, flash, jsonify, session
 from app.models import Product, User
 from app.forms import LoginForm, RegisterForm
 from flask_login import login_user, logout_user, login_required, current_user
 import datetime
 import requests
 
+def checkCartSession():
+    try:
+        return session['cart']
+    except:
+        session['cart'] = []
+
+@app.context_processor
+def getGlobal():
+    try:
+        return {'sessionCart': session['cart']}
+    except:
+        session['cart'] = []
+    return {'sessionCart': session['cart']}
+
 @app.route('/')
 @app.route('/index', methods={'GET'})
 def index():
+    checkCartSession()
+    session['cart'] = []
     products = Product.query.all()
     return render_template('index.html', title="Home", products=products)
 
 @app.route('/products', methods={'GET'})
 def products():
+    # checkCartSession()
     products = Product.query.all()
+    print(session['cart'])
     return render_template('products.html', title="Products", products=products)
 
+@app.route('/products/add/<id>', methods=['GET', 'POST'])
+def productsAdd(id):
+    # checkCartSession()
+    product = Product.query.get(id)
+    item = {'id': product.id,
+            'name': product.name,
+            'image': product.image,
+            'price': product.price,
+            'description': product.description
+            }
+
+    session['cart'].append(item)
+
+    flash(f"{item['name']} added to cart")
 
 
 
+    return redirect(url_for('products'))
+
+@app.route('/products/view', methods=['GET', 'POST'])
+def products_view():
+
+    total = 0
+    for item in session['cart']:
+        total += item['price']
+
+    total = round(total, 2)
+
+    return render_template('products_view.html', total=total)
 
 @app.route('/new_arrivals', methods={'GET'})
 def new_arrivals():
-
+    checkCartSession()
     URL = 'http://api.shortboxed.com/comics/v1/new'
     r = requests.get(URL)
     new_arrivals = r.json()
@@ -36,7 +80,7 @@ def new_arrivals():
 
 @app.route('/login', methods={'GET', 'POST'})
 def login():
-
+    checkCartSession()
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -57,6 +101,7 @@ def login():
 
 @app.route('/register', methods={'GET', 'POST'})
 def register():
+    checkCartSession()
     # if user is already logged in , redirect them.
     if current_user.is_authenticated:
         flash("you are already logged in")
@@ -87,6 +132,7 @@ def register():
 @app.route('/profile')
 @app.route('/profile/<username>', methods=['GET', 'POST'])
 def profile(username=''):
+    checkCartSession()
     # if username is empty
     if not username:
         return redirect(url_for('login'))
@@ -97,5 +143,7 @@ def profile(username=''):
 
 @app.route('/logout')
 def logout():
+    checkCartSession()
+
     logout_user()
     return redirect(url_for('login'))
---
+++ app/static/js/app.js
@@ -0,0 +1,3 @@
+// function addItem(id) {
+//     alert(id);
+// }
